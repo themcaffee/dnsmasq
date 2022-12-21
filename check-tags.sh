f3f410e62825950c1ed79937510212f456fdadc8
Mitch McAffee <themcaffee@gmail.com>
2022-12-21 02:37:05 -0800

    work on tags action

--- check-tags.sh
+++ check-tags.sh
@@ -4,25 +4,26 @@
 # Configure git
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-# Get dnsmasq
-git clone git://thekelleys.org.uk
-# Get dnsmasq tags
-cd /dnsmasq
-git fetch --all --tags
-git tag --list 'v*' | sort -V > /tmp/latest-tag
 # Get the tags that exist in this repo
-cd /dnsmasq-docker
 git fetch --all --tags
-git tag --list 'v*' | sort -V > /tmp/latest-tag-docker
+git tag --list 'v*' | sort -V > ../latest-tag-docker
+# Get dnsmasq tags
+cd ../
+git clone git://thekelleys.org.uk/dnsmasq.git 
+cd dnsmasq
+git fetch --all --tags
+git tag --list 'v*' | sort -V | tail -n 4 > ../latest-tag
+cd ../
 # Iterate through the tags and check if they exist in the docker repo
 while read version; do
     echo $version
-    if grep -q -F -f /tmp/latest-tag-docker $version; then
+    if grep -q -F -f latest-tag-docker $version; then
         echo "No new tag"
     else
         echo "New tag found"
-        cd /dnsmasq-docker
+        cd dnsmasq-docker
         git tag $version
         git push origin $version
+        cd ../
     fi
-done </tmp/latest-tag
+done </./latest-tag
